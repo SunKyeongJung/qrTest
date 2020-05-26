45e11dd66bac4ac97f54701bbc3364cfba85b3a0
SunKyeongJung <tjsrud0314@gmail.com>
2020-05-26 14:20:02 +0900

    add toast

---
+++ js/common.js
@@ -0,0 +1,13 @@
+function toast(string) {
+    const toast = document.getElementById("toast");
+debugger;
+    toast.classList.contains("reveal") ?
+        (clearTimeout(removeToast), removeToast = setTimeout(function () {
+            document.getElementById("toast").classList.remove("reveal")
+        }, 1000)) :
+        removeToast = setTimeout(function () {
+            document.getElementById("toast").classList.remove("reveal")
+        }, 1000)
+    toast.classList.add("reveal"),
+        toast.innerText = string
+}
